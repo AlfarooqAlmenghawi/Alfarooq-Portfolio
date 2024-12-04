7b7f72ca2268235db651f9afbfdf9fc48a3c70c8
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2024-12-04 15:19:48 +0000

    forgot a change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./Components/Header.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
