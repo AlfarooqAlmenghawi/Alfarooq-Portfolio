026ad1b6432b00ab96208f156ef9e66f59b79bfe
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2024-12-10 19:51:28 +0000

    Add animated title and work on the home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,7 +1,39 @@
 import "./HomePage.css";
+import { TypeAnimation } from "react-type-animation";
 
 const HomePage = () => {
-  return <h1 className="section-title">Home Page</h1>;
+  return (
+    <main>
+      <section className="section-title-sizing">
+        <TypeAnimation
+          className="section-title"
+          sequence={[
+            "Welcome to Alfarooq Almenghawi's Portfolio",
+            3000,
+            "",
+            1000,
+            "Graduated from Northcoders, aspiring to shape the tech industry",
+            3000,
+            "",
+            1000,
+            "Specializing in JavaScript, React, and backend development",
+            3000,
+            "",
+            1000,
+            "Let's build something amazing together!",
+            3000,
+            "",
+            1000,
+          ]}
+          wrapper="span"
+          speed={60}
+          style={{}}
+          repeat={Infinity}
+        />
+      </section>
+      <p className="section-information"></p>
+    </main>
+  );
 };
 
 export default HomePage;
