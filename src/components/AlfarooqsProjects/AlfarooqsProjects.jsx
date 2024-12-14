233a47ea729a5aa7f3a174a14166eae7ca52ad22
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2024-12-14 19:44:25 +0000

    Add late plate project information plus a line break with a hr element and duplication of the information below again for testing purposes

--- src/components/AlfarooqsProjects/AlfarooqsProjects.jsx
+++ src/components/AlfarooqsProjects/AlfarooqsProjects.jsx
@@ -1,5 +1,19 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./AlfarooqsProjects.css";
+
+const arrowIcon = (
+  <>
+    <svg
+      className="arrow-icon"
+      fill="currentColor"
+      viewBox="0 0 7 12"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 10.6L4.2 6 0 1.4 1.3 0l5.5 6-5.5 6L0 10.6z"></path>
+    </svg>
+  </>
+);
 
 const AlfarooqsProjects = () => {
   useEffect(() => {
@@ -36,7 +50,79 @@ const AlfarooqsProjects = () => {
       <div id="particles-js-projects" className="background"></div>
       <section>
         <h1 className="projects-section-title">Alfarooqs Projects</h1>
-        <summary></summary>
+        <summary>
+          <section className="late-plate-section">
+            <h2 className="late-plate-title">
+              Late Plate - Restaurant Availability Web Application
+            </h2>
+            <div className="late-plate-main-content">
+              <img
+                className="late-plate-image"
+                src="/assets/projects-backdrops/late-plate.png"
+              />
+              <div className="descriptions-and-links">
+                <p className="late-plate-description">
+                  Created as a group of 7, this is a Restaurant Availability App
+                  for people who want to eat out but have missed the chance to
+                  book a table in advance. It uses the user’s location to give
+                  them access to the restaurants near them and make a booking
+                  for a table on the same day once a restaurant catches their
+                  eye. We also created a Restaurant Staff side web app where the
+                  staff can create and manage restaurants, tables and bookings
+                  made by customers.
+                </p>
+                <Link
+                  className="projects-links-button"
+                  to="https://www.youtube.com/watch?v=9Olc02apbR8"
+                >
+                  Late Plate Project Demo Video {arrowIcon}
+                </Link>
+                <Link
+                  className="projects-links-button"
+                  to="https://github.com/AlfarooqAlmenghawi/Restaurant_Staff_Website"
+                >
+                  GitHub Repository {arrowIcon}
+                </Link>
+              </div>
+            </div>
+          </section>
+          <hr />
+          <section className="late-plate-section">
+            <h2 className="late-plate-title">
+              Late Plate - Restaurant Availability Web Application
+            </h2>
+            <div className="late-plate-main-content">
+              <img
+                className="late-plate-image"
+                src="/assets/projects-backdrops/late-plate.png"
+              />
+              <div className="descriptions-and-links">
+                <p className="late-plate-description">
+                  Created as a group of 7, this is a Restaurant Availability App
+                  for people who want to eat out but have missed the chance to
+                  book a table in advance. It uses the user’s location to give
+                  them access to the restaurants near them and make a booking
+                  for a table on the same day once a restaurant catches their
+                  eye. We also created a Restaurant Staff side web app where the
+                  staff can create and manage restaurants, tables and bookings
+                  made by customers.
+                </p>
+                <Link
+                  className="projects-links-button"
+                  to="https://www.youtube.com/watch?v=9Olc02apbR8"
+                >
+                  Late Plate Project Demo Video {arrowIcon}
+                </Link>
+                <Link
+                  className="projects-links-button"
+                  to="https://github.com/AlfarooqAlmenghawi/Restaurant_Staff_Website"
+                >
+                  GitHub Repository {arrowIcon}
+                </Link>
+              </div>
+            </div>
+          </section>
+        </summary>
       </section>
     </main>
   );
