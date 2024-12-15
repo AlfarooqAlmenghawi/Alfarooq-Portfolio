2894e3a62de99facaa09da96c398ab90e2e3b243
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2024-12-15 23:13:25 +0000

    Complete contacts page with alfarooqs contact details + minor tweakings to hover on the styling buttons

--- src/components/ContactAlfarooq/ContactAlfarooq.jsx
+++ src/components/ContactAlfarooq/ContactAlfarooq.jsx
@@ -1,5 +1,59 @@
+import { useEffect } from "react";
+import "./ContactAlfarooq.css";
+
 const ContactAlfarooq = () => {
-  return <p>Contact Alfarooq</p>;
+  useEffect(() => {
+    // Dynamically load the particles.js library
+    const script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js";
+    script.onload = () => {
+      // Initialize particles.js after the script is loaded
+      window.particlesJS("particles-js-contact", {
+        particles: {
+          number: { value: 100 },
+          size: { value: 3 },
+          move: { speed: 3 },
+          color: { value: "#C9CB06" }, // Set particle color here
+          line_linked: { color: "#C9CB06" }, // Link line color
+        },
+        interactivity: {
+          events: {
+            onhover: { enable: true, mode: "repulse" },
+          },
+        },
+      });
+    };
+    document.body.appendChild(script);
+
+    // Cleanup script element on component unmount
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <main className="contact-page-content">
+      <div id="particles-js-contact" className="background"></div>
+      <section>
+        <h1 className="contact-section-title">Contact Alfarooq</h1>
+        <summary>
+          <p className="cridentials-information">
+            If you would like to reach out to provide a job opportunity, talk
+            about my work and experience or even speak together about all things
+            tech and programming (and potential project collaboration), I have
+            provided the means to contact me below! I am also contactable on{" "}
+            <i>WhatsApp</i> on the same number.
+          </p>
+          <p className="cridentials">
+            <b>Email:</b> a.almenghawi123@gmail.com
+          </p>
+          <p className="cridentials">
+            <b>Phone Number:</b> 07869 417307
+          </p>
+        </summary>
+      </section>
+    </main>
+  );
 };
 
 export default ContactAlfarooq;
